9ea8b42716f5a2fea43929e7d0396dd1ab2d9bd4
Sean McCarty <28798809+seanmccarty@users.noreply.github.com>
2022-09-25 14:09:07 -0400

    Add RR rolls to combat drop manager. Enables dragging RR trigger buttons onto the CT

---
+++ scripts/rr_manager_combat_drop.lua
@@ -0,0 +1,16 @@
+function onInit()
+	CombatDropManager.setDragTypeDropCallback("RR", RRCombatDropManager.onRRDragTypeDrop);
+end
+
+---This allows RR roll triggers to be dragged onto the actual combat tracker as valid drag and drops
+---@param tCustom any
+---@return boolean
+function onRRDragTypeDrop(tCustom)
+	local rTarget = ActorManager.resolveActor(tCustom.sTargetPath);
+	if not rTarget then
+		return false;
+	end
+	local sType = tCustom.draginfo.getDescription();
+	RRRollManager.onButtonPress(sType,tCustom.sTargetPath);
+	return true;
+end
