639aaffc46e89bf74729002b65d38438a9bf8874
Sean McCarty <28798809+seanmccarty@users.noreply.github.com>
2023-03-29 20:26:32 -0400

    Fix DataCommon errors for building default rolls

--- console/scripts/rr_console_options.lua
+++ console/scripts/rr_console_options.lua
@@ -6,15 +6,17 @@
 function onInit()
 	registerMenuItem(Interface.getString("list_menu_createitem"), "insert", 5);
 	
-	-- Construct default skills
-	if self.datasource[1] == ".skill.list" then
-		constructDefaultSkills();
-	end
-	if self.datasource[1] == ".check.list" then
-		constructDefaultChecks();
-	end
-	if self.datasource[1] == ".save.list" then
-		constructDefaultSaves();
+	-- Construct default skills, if DataCommon does not exist, no way to build default list for special rolls
+	if DataCommon then
+		if self.datasource[1] == ".skill.list" then
+			constructDefaultSkills();
+		end
+		if self.datasource[1] == ".check.list" then
+			constructDefaultChecks();
+		end
+		if self.datasource[1] == ".save.list" then
+			constructDefaultSaves();
+		end
 	end
 	if self.datasource[1] == ".dice.list" then
 		constructDefaultDice();
