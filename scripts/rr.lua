177f0fc0d138f7359c4721407ee546af0de16f40
Sean McCarty <28798809+seanmccarty@users.noreply.github.com>
2024-11-03 21:09:33 -0600

    Work in progress Migrate to roll types being sub nodes so they can be shown in a windowlist Adopt changes to remove fastinit from RR console

--- scripts/rr.lua
+++ scripts/rr.lua
@@ -10,6 +10,58 @@ function onInit()
 	registerSlashHandlers();
 	initializeDirtyState();
 	runMigration();
+	buildComboOptions();
+end
+
+function buildComboOptions()
+	if DataCommon then
+		DB.setValue("requestsheet.rolls.check.display_name", "string", "Check");
+		DB.setValue("requestsheet.rolls.save.display_name", "string", "Save");
+		DB.setValue("requestsheet.rolls.skill.display_name", "string", "Skill");
+		DB.setValue("requestsheet.rolls.dice.display_name", "string", "Die");
+
+		DB.setValue("requestsheet.rolls.check.sort_order", "number", 1);
+		DB.setValue("requestsheet.rolls.save.sort_order", "number", 2);
+		DB.setValue("requestsheet.rolls.skill.sort_order", "number", 3);
+		DB.setValue("requestsheet.rolls.dice.sort_order", "number", 4);
+		if DataCommon.psabilitydata and DB.findNode("requestsheet.rolls.check.list") == nil then
+			local node = DB.createNode("requestsheet.rolls.check.list");
+			for _,w in ipairs(DataCommon.psabilitydata) do
+				local node2 = DB.createChild(node);
+				DB.setValue(node2,"name","string",w);
+				DB.setValue(node2, "show", "number", "2");
+			end
+		end
+		-- TODO fix to include rulesets other than 5E
+		if DataCommon.psabilitydata and DB.findNode("requestsheet.rolls.save.list") == nil then
+			local node = DB.createNode("requestsheet.rolls.save.list");
+			for _,w in ipairs(DataCommon.psabilitydata) do
+				local node2 = DB.createChild(node);
+				DB.setValue(node2,"name","string",w);
+				DB.setValue(node2, "show", "number", "2");
+			end
+		end
+
+		if DataCommon.skilldata and DB.findNode("requestsheet.rolls.skill.list") == nil then
+			local node = DB.createNode("requestsheet.rolls.skill.list");
+			for k, _ in pairs(DataCommon.skilldata) do
+				local node2 = DB.createChild(node);
+				DB.setValue(node2,"name","string",k);
+				DB.setValue(node2, "show", "number", "2");
+			end
+		end
+
+		if DB.findNode("requestsheet.rolls.dice.list") == nil then
+			local node = DB.createNode("requestsheet.rolls.dice.list");
+			local dice = {"d4","d6","d8","d10","d20"};
+			for _,w in ipairs(dice) do
+				local node2 = DB.createChild(node);
+				DB.setValue(node2,"name","string",w);
+				DB.setValue(node2, "show", "number", "2");
+			end
+		end
+
+	end
 end
 
 --#region options manager
@@ -365,8 +417,9 @@ function notifyApplyDirty(sCTNode, isDirty)
 	Comm.deliverOOBMessage(msgOOB, "");
 end
 
----Migrates the nodes to data structure v2.3
+---Migrates the nodes to data structure v2.3 and then 2.17
 function runMigration()
+	-- 2.3
 	migrateNode("requestsheet.checklist","requestsheet.check.list" );
 	migrateNode("requestsheet.savelist","requestsheet.save.list" );
 	migrateNode("requestsheet.skilllist","requestsheet.skill.list" );
@@ -381,6 +434,11 @@ function runMigration()
 	migrateNode("requestsheet.skillselected","requestsheet.skill.selected");
 	migrateNode("requestsheet.skilllistcollapsed","requestsheet.skill.collapsed");
 
+	-- 2.17
+	migrateNode("requestsheet.check.list","requestsheet.rolls.check.list");
+	migrateNode("requestsheet.save.list","requestsheet.rolls.save.list");
+	migrateNode("requestsheet.skill.list","requestsheet.rolls.skill.list");
+	migrateNode("requestsheet.dice.list","requestsheet.rolls.dice.list");
 end
 
 ---Copies a source node to the destination and deletes the source node
