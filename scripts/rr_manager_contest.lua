088493e3de519f0d49d5cd11f4fbb3b3b78c4905
Sean McCarty <28798809+seanmccarty@users.noreply.github.com>
2025-12-10 14:43:48 -0600

    Add picking best roll for each character Had to override EffectManager.notifyExpire so I could check for best nMod without expiring effects

--- scripts/rr_manager_contest.lua
+++ scripts/rr_manager_contest.lua
@@ -1,29 +1,88 @@
+local fOriginalEffectNotifyExpire = nil;
+local bSuspend = false;
 function onInit()
 	if Session.IsHost then
 		DB.createNode("requestsheet.contest");
 		DB.setPublic("requestsheet.contest",true);
 	end
-
+	fOriginalEffectNotifyExpire = EffectManager.notifyExpire;
+	EffectManager.notifyExpire = RREffectNotifyExpire;
 	-- insert into list of Actions and targetactions so that CombatDropManager and ActionsManager.actionDrop will process drops on the combat tracker
 	ActionsManager.initAction("contest");
 	table.insert(GameSystem.targetactions, "contest");
+
+	registerModGetter("skill",ActionSkill.modRoll);
+	registerModGetter("init",ActionInit.modRoll);
+	registerModGetter("check",ActionCheck.modRoll);
+	registerModGetter("save", ActionSave.modRoll);
+end
+
+---Replace EffectManager.notifyExpire so that I can use the various modRolls without expiring effects that end up being unused
+function RREffectNotifyExpire(varEffect, nMatch, bImmediate)
+	if not bSuspend then
+		fOriginalEffectNotifyExpire(varEffect, nMatch, bImmediate);
+	end
+end
+
+local aModHandlers = {};
+function registerModGetter(sActionType, callback)
+	aModHandlers[sActionType] = callback;
 end
 
 function getRollType(sNode)
 	return DB.getValue(sNode..".rollType","");
 end
 
--- TODO not just first
-function getFirstTargetSubType(sNode)
-	return DB.getValue(sNode..".subtypes.id-00002.type","");
+function getSubTypes(sNode, bIsOriginator)
+	local sComparator = "target";
+	if bIsOriginator then
+		sComparator = "originator";
+	end
+	local tSubTypes = {};
+	for _,v in pairs(DB.getChildren(sNode..".subtypes")) do
+		if DB.getValue(v,"origin","")==sComparator then
+			table.insert(tSubTypes,DB.getValue(v,"type",""));
+		end
+	end
+	return tSubTypes;
 end
-function getFirstOriginSubType(sNode)
-	return DB.getValue(sNode..".subtypes.id-00001.type","");
+
+function getBestSubType(rActor,sType,tSubTypes)
+	if not tSubTypes then 
+		return nil;
+	end
+	if #tSubTypes==1 then
+		return tSubTypes[1];
+	end
+	local fRollResult = RRRollManager.getRollGetter(sType);
+	local fModResult = aModHandlers[sType];
+	ActionsManager.lockModifiers()
+	bSuspend = true;
+	local sBest = "";
+	local nBest = -99;
+	for _,sSubType in pairs(tSubTypes) do
+			
+		local rRoll = fRollResult(rActor, sSubType);
+		if fModResult then
+			fModResult(rActor, nil, rRoll);
+		end
+
+		if rRoll.nMod>nBest then
+			sBest = sSubType;
+			nBest = rRoll.nMod;
+		end
+	end
+	ActionsManager.unlockModifiers(false);
+	bSuspend = false;
+	return sBest;
 end
 
 function setupContestRoll(contestNode, rActor)
-	local fRollResult = RRRollManager.getRollGetter(RRContestManager.getRollType(contestNode));
-	local rRoll = fRollResult(rActor, RRContestManager.getFirstOriginSubType(contestNode));
+	local sType = RRContestManager.getRollType(contestNode);
+	local tSubTypes = RRContestManager.getSubTypes(contestNode,true);
+	local sBestSubType = RRContestManager.getBestSubType(rActor, sType, tSubTypes);
+	local fRollResult = RRRollManager.getRollGetter(sType);
+	local rRoll = fRollResult(rActor, sBestSubType);
 	rRoll.bContest = true;
 	rRoll.contestNode = contestNode;
 
@@ -52,9 +111,8 @@ end
 
 function finishContest(rSource, rTarget, rRoll)
 	if rRoll.bContest then
-		local rollType = rRoll.sType;
-		local subType = RRContestManager.getFirstTargetSubType(rRoll.contestNode);
-		-- TODO rolls without a subType, such as initiative
+		local sType = rRoll.sType;
+		local tSubTypes = RRContestManager.getSubTypes(rRoll.contestNode,false);
 		local nTotal = ActionsManager.total(rRoll);
 		local rTargets = {};
 		if rTarget then
@@ -65,7 +123,12 @@ function finishContest(rSource, rTarget, rRoll)
 				table.insert(rTargets, DB.getPath(v));
 			end
 		end
-		RRRollManager.requestRoll(rollType, subType, rTargets, rRoll.bSecret, nTotal, "Contest with "..ActorManager.getDisplayName(rSource).." vs DC "..tostring(nTotal));
+		-- Process each target individually since they may all have different best subTypes
+		for _,rTarget in pairs(rTargets) do
+			local sBestSubType = RRContestManager.getBestSubType(rTarget, sType, tSubTypes);
+			RRRollManager.requestRoll(sType, sBestSubType, {rTarget}, rRoll.bSecret, nTotal, "Contest with "..ActorManager.getDisplayName(rSource).." vs DC "..tostring(nTotal));
+		end
+		
 		-- Debug.chat("old", rRoll,rTarget, "rSource",rSource)
 	end
 end
